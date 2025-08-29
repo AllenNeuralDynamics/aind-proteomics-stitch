9f0ab06ffd375f787678356c9a53ea856a0b1950
cbrrrry <carson.berry@alleninstitute.org>
2025-08-29 23:41:59 +0000

    Edited combine_stitching_and_cam_alignment.py

--- code/aind_proteomics_stitch/combine_stitching_and_cam_alignment.py
+++ code/aind_proteomics_stitch/combine_stitching_and_cam_alignment.py
@@ -1,4 +1,6 @@
 from .utils.xml_utils import transfer_stitching_to_multichannel
+from . import __maintainers__, __pipeline_version__, __version__
+from .utils import create_nominal_positions, utils
 
 def combine_xmls():
     output_big_stitcher_xml = '/results/bigstitcher.xml'
