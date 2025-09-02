b1d5ff8d2f0bf260d3ba5b1e95f917723ed18687
cbrrrry <carson.berry@alleninstitute.org>
2025-09-02 18:21:05 +0000

    add single channel splitting to top level run

--- code/combine_stitching_and_cam_alignment.py
+++ code/combine_stitching_and_cam_alignment.py
@@ -4,9 +4,20 @@ def combine_xmls():
     output_big_stitcher_xml = '../results/bigstitcher.xml'
     CAMERA_ALIGNED_XML_PATH =  "../data/stitching_cam_alignment_spot_channels.xml"
     COMBINED_XML_PATH = "../results/combined_stitching_cam_alignment_all_channels.xml"
+    
     transfer_stitching_to_multichannel(single_channel_xml = output_big_stitcher_xml, 
     multichannel_xml = CAMERA_ALIGNED_XML_PATH, 
     output_xml = COMBINED_XML_PATH)
 
+    
+def split_xmls_to_single_channel_xmls():
+    output_files = split_multichannel_xml(
+        xml_path="../data/stitching_cam_alignment_spot_channels.xml",
+        output_dir="../results/single_channel_xmls"
+    )
+    print(f"Created {len(output_files)} channel-specific XMLs")
+
+
 if __name__ == "__main__": 
     combine_xmls()
+    split_xmls_to_single_channel_xmls()
