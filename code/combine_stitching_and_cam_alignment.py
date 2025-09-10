92e5c84aed28ab33ffe36e47432cd533a8dc0922
cbrrrry <carson.berry@alleninstitute.org>
2025-09-10 19:09:08 +0000

    try catch and forward transform

--- code/combine_stitching_and_cam_alignment.py
+++ code/combine_stitching_and_cam_alignment.py
@@ -2,20 +2,27 @@ from aind_proteomics_stitch.utils.xml_utils import transfer_stitching_to_multich
 
 def combine_xmls():
     output_big_stitcher_xml = '../results/bigstitcher.xml'
-    CAMERA_ALIGNED_XML_PATH =  "../data/stitching_cam_alignment_spot_channels.xml"
+    CAMERA_ALIGNED_XML_PATH =  "../data/stitching_cam_alignment_forward_transform_spot_channels.xml"
     COMBINED_XML_PATH = "../results/combined_stitching_cam_alignment_all_channels.xml"
 
-    transfer_stitching_to_multichannel(single_channel_xml = output_big_stitcher_xml, 
-    multichannel_xml = CAMERA_ALIGNED_XML_PATH, 
-    output_xml = COMBINED_XML_PATH)
+    try: 
+        transfer_stitching_to_multichannel(single_channel_xml = output_big_stitcher_xml, 
+        multichannel_xml = CAMERA_ALIGNED_XML_PATH, 
+        output_xml = COMBINED_XML_PATH)
+    except: 
+        print(f'Error combining xmls')
 
     
 def split_xmls_to_single_channel_xmls():
-    output_files = split_multichannel_xml(
-        xml_path="../data/stitching_cam_alignment_forward_transform_spot_channels.xml",
-        output_dir="../results/single_channel_xmls"
-    )
-    print(f"Created {len(output_files)} channel-specific XMLs")
+    
+    try:
+        output_files = split_multichannel_xml(
+            xml_path="../data/stitching_cam_alignment_forward_transform_spot_channels.xml",
+            output_dir="../results/single_channel_xmls"
+        )
+        print(f"Created {len(output_files)} channel-specific XMLs")
+    except: 
+        print(f'Error splitting xmls')
 
 
 if __name__ == "__main__": 
